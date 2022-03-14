2f4c9241f5da4ad1013a6404e0b0ad9216f34138
AtlantaPatterson <atlantarpatterson@gmail.com>
2022-03-14 15:03:58 -0700

    commit tree c640dd7ff0ecb458afc526985003f1db9c026b64

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(number){
+  if (number <= 400) {
+    return "This one is on me!"
+  } 
+  else if (number > 2500) {
+    return "No can do."
+  }  
+  else if (number > 2000) {
+    return "I will gladly take your thirty bucks."
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  if (city === "NYC") {
+    return "Ok, sounds good."
+  }
+  else {
+    return "No go."
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case "generous":
+      return "Thank you so much."
+    case "not as generous":
+      return "Thank you."
+    default:
+    return "Bye."
+  }
 }
